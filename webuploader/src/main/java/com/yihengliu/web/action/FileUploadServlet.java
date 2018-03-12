2d570f0da07aa983cef2e576b5275ffe8144d592
sunfx <123074528@qq.com>
2018-03-12 20:22:15 +0800

    更新
    Signed-off-by: sunfx <123074528@qq.com>

--- webuploader/src/main/java/com/yihengliu/web/action/FileUploadServlet.java
+++ webuploader/src/main/java/com/yihengliu/web/action/FileUploadServlet.java
@@ -3,94 +3,94 @@ package com.yihengliu.web.action;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Servlet user to accept file upload
  */
 public class FileUploadServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	private String serverPath = "c:\\temp";
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+    private String serverPath = "c:\\temp";
 
-		System.out.println("进入后台...");
+    private Log log = LogFactory.getLog(getClass());
 
-		// 1.创建DiskFileItemFactory对象，配置缓存用
-		DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        response.getWriter().append("Served at: ").append(request.getContextPath());
 
-		// 2. 创建 ServletFileUpload对象
-		ServletFileUpload servletFileUpload = new ServletFileUpload(diskFileItemFactory);
+        log.debug("进入后台...");
 
-		// 3. 设置文件名称编码
-		servletFileUpload.setHeaderEncoding("utf-8");
+        // 1.创建DiskFileItemFactory对象，配置缓存用
+        DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
 
-		// 4. 开始解析文件
-		// 文件md5获取的字符串
-		String fileMd5 = null;
-		// 文件的索引
-		String chunk = null;
-		try {
-			List<FileItem> items = servletFileUpload.parseRequest(request);
-			for (FileItem fileItem : items) {
+        // 2. 创建 ServletFileUpload对象
+        ServletFileUpload servletFileUpload = new ServletFileUpload(diskFileItemFactory);
 
-				if (fileItem.isFormField()) { // >> 普通数据
-					String fieldName = fileItem.getFieldName();
-					if ("info".equals(fieldName)) {
-						String info = fileItem.getString("utf-8");
-						System.out.println("info:" + info);
-					}
-					if ("fileMd5".equals(fieldName)) {
-						fileMd5 = fileItem.getString("utf-8");
-						System.out.println("fileMd5:" + fileMd5);
-					}
-					if ("chunk".equals(fieldName)) {
-						chunk = fileItem.getString("utf-8");
-						System.out.println("chunk:" + chunk);
-					}
-				} else { // >> 文件
-					/*// 1. 获取文件名称
-					String name = fileItem.getName();
-					// 2. 获取文件的实际内容
-					InputStream is = fileItem.getInputStream();
-					
-					// 3. 保存文件
-					FileUtils.copyInputStreamToFile(is, new File(serverPath + "/" + name));*/
+        // 3. 设置文件名称编码
+        servletFileUpload.setHeaderEncoding("utf-8");
 
-					// 如果文件夹没有创建文件夹
-					File file = new File(serverPath + "/" + fileMd5);
-					if (!file.exists()) {
-						file.mkdirs();
-					}
-					// 保存文件
-					File chunkFile = new File(serverPath + "/" + fileMd5 + "/" + chunk);
-					FileUtils.copyInputStreamToFile(fileItem.getInputStream(), chunkFile);
+        // 4. 开始解析文件
+        // 文件md5获取的字符串
+        String fileMd5 = null;
+        // 文件的索引
+        String chunk = null;
+        try {
+            List<FileItem> items = servletFileUpload.parseRequest(request);
+            for (FileItem fileItem : items) {
 
-				}
+                if (fileItem.isFormField()) { // >> 普通数据
+                    String fieldName = fileItem.getFieldName();
+                    if ("info".equals(fieldName)) {
+                        String info = fileItem.getString("utf-8");
+                        log.debug("info:" + info);
+                    }
+                    if ("fileMd5".equals(fieldName)) {
+                        fileMd5 = fileItem.getString("utf-8");
+                        log.debug("fileMd5:" + fileMd5);
+                    }
+                    if ("chunk".equals(fieldName)) {
+                        chunk = fileItem.getString("utf-8");
+                        log.debug("chunk:" + chunk);
+                    }
+                } else { // >> 文件
+                    /*
+                     * // 1. 获取文件名称 String name = fileItem.getName(); 
+                     * // 2. 获取文件的实际内容 InputStream is = fileItem.getInputStream();
+                     * // 3. 保存文件 FileUtils.copyInputStreamToFile(is, new File(serverPath + "/" + name));
+                     */
 
-			}
+                    // 如果文件夹没有创建文件夹
+                    File file = new File(serverPath + "/" + fileMd5);
+                    if (!file.exists()) {
+                        file.mkdirs();
+                    }
+                    // 保存文件
+                    File chunkFile = new File(serverPath + "/" + fileMd5 + "/" + chunk);
+                    FileUtils.copyInputStreamToFile(fileItem.getInputStream(), chunkFile);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+                }
 
-	}
+            }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
+        } catch (Exception e) {
+            log.debug(e.getMessage(), e);
+        }
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 
 }
--- webuploader/src/main/java/com/yihengliu/web/action/UploadActionServlet.java
+++ webuploader/src/main/java/com/yihengliu/web/action/UploadActionServlet.java
@@ -17,27 +17,33 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * 合并上传文件
  */
 public class UploadActionServlet extends HttpServlet {
+    
 	private static final long serialVersionUID = 1L;
 
 	private String serverPath = "c:\\temp";
 
+    private Log log = LogFactory.getLog(getClass());
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("进入合并后台...");
+	    
+	    log.debug("进入合并后台...");
+	    
 		String action = request.getParameter("action");
+		
 		if ("mergeChunks".equals(action)) {
 			// 获得需要合并的目录
 			String fileMd5 = request.getParameter("fileMd5");
-
 			// 读取目录所有文件
 			File f = new File(serverPath + "/" + fileMd5);
 			File[] fileArray = f.listFiles(new FileFilter() {
-
 				// 排除目录，只要文件
 				@Override
 				public boolean accept(File pathname) {
@@ -52,7 +58,6 @@ public class UploadActionServlet extends HttpServlet {
 			List<File> fileList = new ArrayList<File>(Arrays.asList(fileArray));
 			// 从小到大排序
 			Collections.sort(fileList, new Comparator<File>() {
-
 				@Override
 				public int compare(File o1, File o2) {
 					if (Integer.parseInt(o1.getName()) < Integer.parseInt(o2.getName())) {
@@ -60,7 +65,6 @@ public class UploadActionServlet extends HttpServlet {
 					}
 					return 1;
 				}
-
 			});
 
 			// 新建保存文件
@@ -79,7 +83,6 @@ public class UploadActionServlet extends HttpServlet {
 				inChannel = new FileInputStream(file).getChannel();
 				inChannel.transferTo(0, inChannel.size(), outChannel);
 				inChannel.close();
-
 				// 删除分片
 				file.delete();
 			}
@@ -94,21 +97,18 @@ public class UploadActionServlet extends HttpServlet {
 				tempFile.delete();
 			}
 
-			System.out.println("合并文件成功");
+			log.debug("合并文件成功");
 
 		} else if ("checkChunk".equals(action)) {
 			// 校验文件是否已经上传并返回结果给前端
-
 			// 文件唯一表示								
 			String fileMd5 = request.getParameter("fileMd5");
 			// 当前分块下标
 			String chunk = request.getParameter("chunk");
 			// 当前分块大小
 			String chunkSize = request.getParameter("chunkSize");
-
 			// 找到分块文件
 			File checkFile = new File(serverPath + "/" + fileMd5 + "/" + chunk);
-
 			// 检查文件是否存在，且大小一致
 			response.setContentType("text/html;charset=utf-8");
 			if (checkFile.exists() && checkFile.length() == Integer.parseInt((chunkSize))) {
